a089d3f7dd743a0a3cbd3c405735f6276400955b
HaoTNG <60647717+HaoTNG@users.noreply.github.com>
2025-04-25 05:03:21 +0700

    Update sys_killall.c

--- src/sys_killall.c
+++ src/sys_killall.c
@@ -9,40 +9,67 @@
  */
 
 #include "common.h"
+#include "queue.h" // Required for empty, enqueue, dequeue functions
 #include "syscall.h"
 #include "stdio.h"
 #include "libmem.h"
+#include <string.h> // Required for strcmp function
+#include <stdlib.h> // Required for free function
 
-int __sys_killall(struct pcb_t *caller, struct sc_regs* regs)
+int __sys_killall(struct pcb_t *caller, struct sc_regs *regs)
 {
-    char proc_name[100];
+    char proc_name[100]; // Holds the name of the process to terminate
     uint32_t data;
+    uint32_t memrg = regs->a1; // Region ID pointing to the memory containing the process name
 
-    //hardcode for demo only
-    uint32_t memrg = regs->a1;
-    
-    /* TODO: Get name of the target proc */
-    //proc_name = libread..
+    /* Read the process name from the specified memory region */
     int i = 0;
     data = 0;
-    while(data != -1){
+    while (data != -1)
+    {
         libread(caller, memrg, i, &data);
-        proc_name[i]= data;
-        if(data == -1) proc_name[i]='\0';
+        proc_name[i] = data;
+        if (data == -1)
+            proc_name[i] = '\0';
         i++;
     }
-    printf("The procname retrieved from memregionid %d is \"%s\"\n", memrg, proc_name);
+    printf("The process name retrieved from memregionid %d is \"%s\"\n", memrg, proc_name);
 
-    /* TODO: Traverse proclist to terminate the proc
-     *       stcmp to check the process match proc_name
-     */
-    //caller->running_list
-    //caller->mlq_ready_queu
+    /* Iterate through the running_list */
+    struct pcb_t *current_proc;
+    while (!empty(caller->running_list))
+    {                                                 // Correct: remove & to pass proper queue_t pointer
+        current_proc = dequeue(caller->running_list); // Correct: remove & to pass proper queue_t pointer
+        if (strcmp(current_proc->path, proc_name) == 0)
+        {
+            printf("Terminating process: %s\n", current_proc->path);
+            free(current_proc); // Free the memory associated with the process
+        }
+        else
+        {
+            enqueue(caller->running_list, current_proc); // Correct: remove & to pass proper queue_t pointer
+        }
+    }
 
-    /* TODO Maching and terminating 
-     *       all processes with given
-     *        name in var proc_name
-     */
+    /* Iterate through mlq_ready_queue if MLQ scheduler is enabled */
+#ifdef MLQ_SCHED
+    for (int prio = 0; prio < MAX_PRIO; prio++)
+    {
+        while (!empty(&caller->mlq_ready_queue[prio]))
+        {                                                                             // Correct: use & to obtain queue_t pointer
+            struct pcb_t *current_mlq_proc = dequeue(&caller->mlq_ready_queue[prio]); // Correct: use & to obtain queue_t pointer
+            if (strcmp(current_mlq_proc->path, proc_name) == 0)
+            {
+                printf("Terminating MLQ process: %s\n", current_mlq_proc->path);
+                free(current_mlq_proc); // Free the memory associated with the process
+            }
+            else
+            {
+                enqueue(&caller->mlq_ready_queue[prio], current_mlq_proc); // Correct: use & to obtain queue_t pointer
+            }
+        }
+    }
+#endif
 
-    return 0; 
+    return 0;
 }
